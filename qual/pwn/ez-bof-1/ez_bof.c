d960c63a500afb6f50554767d5e229dc0025c77e
nauliajati <nauliajati@gmail.com>
2022-03-10 14:18:38 +0700

    compile

--- qual/pwn/ez-bof-1/ez_bof.c
+++ qual/pwn/ez-bof-1/ez_bof.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
-// gcc -fno-stack-protector -z execstack -o bug bug.c
 
 int main(int argc, char const *argv[])
 {
@@ -14,8 +12,7 @@ int main(int argc, char const *argv[])
     if (name > 0x64)
         system("echo \"Hi, %s. Selamat Datang di Tangerang Kota CTF 2022!\n flagnya :)\"; cat flag.txt");
     else
-        puts("Ok thanks");
+        puts("Ok thanks :)");
     return 0;
-    // printf("Hi, %s. Selamat Datang di Tangerang Kota CTF 2022!\n", name);
-    // return 0;
+
 }
