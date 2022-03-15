e630c6f27387d31cc67ee515394781aceb7948b8
nauliajati <nauliajati@gmail.com>
2022-03-16 03:14:47 +0700

    ubah soal:(

--- qual/Pwn/ez-bof-1/ez-pwn.c
+++ qual/Pwn/ez-bof-1/ez-pwn.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int main(int argc, char **argv)
+int main(int argc, char const *argv[])
 {
-    char nama[100];
-    puts("Siapa Nama Kamu?");
-    gets(nama);
-    if (nama > 100)
-        printf("Hi, %s. Selamat datang di Tangerang Kota CTF 2022 :)\n", nama);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    int hack_me = 0x64;
+    char buf[100];
+
+    puts("Masukan Angka (Max 10 digits)");
+    gets(buf);
+
+    if (hack_me > 0x64)
+        system("echo \"Hi, here is your flag: \"; cat flag.txt");
     else
         puts("Nope :)");
+    return 0;
 }
