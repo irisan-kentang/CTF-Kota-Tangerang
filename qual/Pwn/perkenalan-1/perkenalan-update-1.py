200049c82d0c9c9801c0c8e9e8cf2ce529f1e14b
nauliajati <nauliajati@gmail.com>
2022-03-15 10:24:53 +0700

    Update soal pwn :(

---
+++ qual/Pwn/perkenalan-1/perkenalan-update-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import string, random, os
+
+
+def main():
+    ranges = 8
+    random_number = "".join(random.SystemRandom().choices(string.digits, k=ranges))
+    for i in range(ranges):
+        my_input = os.system(input("Masukan Angka: "))  # be carefull with this function
+        print("Anda memilih: ", my_input)
+        if my_input == random_number:
+            with open("flag.txt", "r") as f:
+                baca = f.read()
+                print("Flag = %s" % (baca))
+                exit(1)
+        else:
+            print("Admin Memilih: ", random_number)
+            print("Tidak Cocok :(\n")
+        break
+
+
+if __name__ == "__main__":
+    main()
