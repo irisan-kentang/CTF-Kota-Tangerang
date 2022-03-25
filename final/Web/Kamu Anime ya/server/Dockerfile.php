6160c85ce0200bf3d2b0948848c914740f341acd
Fadli Maulana <wetmanz23@gmail.com>
2022-03-25 23:49:34 +0700

    Update Dockerfile.php

--- final/Web/Kamu Anime ya/server/Dockerfile.php
+++ final/Web/Kamu Anime ya/server/Dockerfile.php
@@ -1,3 +1,13 @@
 FROM php:7.4-fpm
 
 ADD apps /var/www/html
+ADD entry.sh /entry.sh
+ADD apps /var/www/html
+RUN chmod 755 /entry.sh
+RUN chmod 777 /var/www/html/init.php
+
+RUN groupadd -g 1000 www
+RUN useradd -u 1000 -ms /bin/bash -g www www
+
+# Change current user to www
+USER www
